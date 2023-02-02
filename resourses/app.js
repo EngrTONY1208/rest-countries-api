8a980e35946e163063df46e5a647120fc04c700d
Engr <anthony.thomas19908@gmail.com>
2023-02-02 19:54:22 +0100

    corrections of rest country api

--- resourses/app.js
+++ resourses/app.js
@@ -9,3 +9,26 @@ filter.addEventListener("click", () =>{
 
 const lightmode =  document.querySelector("toggle-container");
 const sunmode = document.querySelector("sun");
+
+
+const darkMode = document.querySelector(".moon-icon");
+const lightMode = document.querySelector(".dark");
+const  toggleLightMenu = document.querySelector(".light-mode")
+   const darkModeContainer = document.querySelector(".dark")
+   lightMode.addEventListener("click", (e) => {
+    if (e.target.matches(".moon-icon") || e.target.parentElement.matches(".moon-icon")) {
+        document.body.classList.add("light-mode")
+        darkMode.innerHTML = `
+        <span class="moon toggle-icon"><i class="fa fa-moon-o"></i>dark mode </span>
+        `
+    }else {
+        document.body.classList.remove("light-mode")
+        darkMode.innerHTML = `
+        <span class="sun toggle-icon"><i class="fa fa-sun-o"></i>light mode </span>
+       `
+      
+    }
+  
+    
+})
+  
